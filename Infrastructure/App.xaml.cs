855135faebeda3681f33388e0381b13450e45c74
Quentium <Quentium@DESKTOP-C7K8053>
2024-12-06 14:47:41 +0000

    RenameAdd project files.

---
+++ Infrastructure/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using WpfAppBase.Views;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+
+namespace WpfAppBase.Infrastructure;
+
+/// <summary>
+///     Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var bootstrapper = new Bootstrapper();
+        bootstrapper.Start();
+
+        MainWindow = new MainWindow();
+        MainWindow.Show();
+    }
+}
---
+++ Infrastructure/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WpfAppBase.Infrastructure;
+
+/// <summary>
+///     Responsible for creating the application builder, and registering all of the dependencies used in dependency injection and configuration.
+/// </summary>
+public class Bootstrapper
+{
+    /// <summary>
+    ///     Builds and runs the host for dependency injection and configuration.
+    /// </summary>
+    public void Start()
+    {
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) => RegisterServices(services))
+            .Build();
+        host.RunAsync();
+    }
+
+    // Register services for dependency injection here
+    private void RegisterServices(IServiceCollection services)
+    {
+
+    }
+}
+
